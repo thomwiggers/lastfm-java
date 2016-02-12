f73e13ec6025937e006a6393fd1858739a83e8ab
Siarhei <siarhei.selivonchyk@gmail.com>
2016-02-12 16:02:39 +0300

    Memory cache logging changed to JUL.

--- src/main/java/de/umass/lastfm/cache/MemoryCache.java
+++ src/main/java/de/umass/lastfm/cache/MemoryCache.java
@@ -35,6 +35,7 @@ import java.io.UnsupportedEncodingException;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.logging.Logger;
 
 /**
  * This class is just for testing. You probably don't want to use it in production.
@@ -42,18 +43,19 @@ import java.util.Map;
  * @author Janni Kovacs
  */
 public class MemoryCache extends Cache {
+	private final Logger log = Logger.getLogger(MemoryCache.class.getName());
 
 	private Map<String, String> data = new HashMap<String, String>();
 	private Map<String, Long> expirations = new HashMap<String, Long>();
 
 	public boolean contains(String cacheEntryName) {
 		boolean contains = data.containsKey(cacheEntryName);
-		System.out.println("MemoryCache.contains: " + cacheEntryName + " ? " + contains);
+		log.info("MemoryCache.contains: " + cacheEntryName + " ? " + contains);
 		return contains;
 	}
 
 	public InputStream load(String cacheEntryName) {
-		System.out.println("MemoryCache.load: " + cacheEntryName);
+		log.info("MemoryCache.load: " + cacheEntryName);
 		try {
 			return new ByteArrayInputStream(data.get(cacheEntryName).getBytes("UTF-8"));
 		} catch (UnsupportedEncodingException e) {
@@ -63,13 +65,13 @@ public class MemoryCache extends Cache {
 	}
 
 	public void remove(String cacheEntryName) {
-		System.out.println("MemoryCache.remove: " + cacheEntryName);
+		log.info("MemoryCache.remove: " + cacheEntryName);
 		data.remove(cacheEntryName);
 		expirations.remove(cacheEntryName);
 	}
 
 	public void store(String cacheEntryName, InputStream inputStream, long expirationDate) {
-		System.out.println("MemoryCache.store: " + cacheEntryName + " Expires at: " + new Date(expirationDate));
+		log.info("MemoryCache.store: " + cacheEntryName + " Expires at: " + new Date(expirationDate));
 		StringBuilder b = new StringBuilder();
 		try {
 			BufferedReader r = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));
@@ -86,7 +88,7 @@ public class MemoryCache extends Cache {
 
 	public boolean isExpired(String cacheEntryName) {
 		boolean exp = expirations.get(cacheEntryName) < System.currentTimeMillis();
-		System.out.println("MemoryCache.isExpired: " + cacheEntryName + " ? " + exp);
+		log.info("MemoryCache.isExpired: " + cacheEntryName + " ? " + exp);
 		return exp;
 	}
 
